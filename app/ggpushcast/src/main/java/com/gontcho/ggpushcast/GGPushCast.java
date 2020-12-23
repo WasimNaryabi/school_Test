8af04d55fdcf3473613c6898a62dae11f9c785cf
WasimNaryabi <waseemnaryabi2018@gmail.com>
2020-12-23 12:58:58 +0500

    First commit

---
+++ app/ggpushcast/src/main/java/com/gontcho/ggpushcast/GGPushCast.java
@@ -0,0 +1,16 @@
+package com.gontcho.ggpushcast;
+
+public class GGPushCast {
+
+    public String checkOddEven(int num){
+        String result;
+        if(num%2 == 0){
+            result = "Even";
+        }
+        else {
+            result = "Odd";
+        }
+        return result;
+    }
+
+}
