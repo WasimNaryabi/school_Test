28db1170656ba603949de7c38f9d5bdc839f73c4
WasimNaryabi <waseemnaryabi2018@gmail.com>
2021-01-12 11:30:22 +0500

    notification received commit

--- app/ggpushcast/src/main/java/com/gontcho/ggpushcast/GGPushCast.java
+++ app/ggpushcast/src/main/java/com/gontcho/ggpushcast/GGPushCast.java
@@ -1,10 +1,12 @@
 package com.gontcho.ggpushcast;
 
+import android.app.Activity;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.media.RingtoneManager;
 import android.net.Uri;
 import android.os.Build;
@@ -35,6 +37,34 @@ public class GGPushCast {
     private String insertUrl = "https://ggpushcast.com/androidsubscribe";
     private String check;
     private String brand, model,  language, country,  versionCode,  versionName,  sdk,  manufacturer;
+
+    public void receiveNotification(Context context, final Class<? extends Activity> ActivityToOpen, String notificationTitle, String notificationMessage, int iconUrl){
+        Intent intent = new Intent(context,ActivityToOpen);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context,1,
+                intent, PendingIntent.FLAG_ONE_SHOT);
+        String channelId = "123456";
+        Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        NotificationCompat.Builder notificationBuilder;
+        notificationBuilder =
+                new NotificationCompat.Builder(context, channelId)
+                        .setSmallIcon(iconUrl)
+                        .setContentTitle(notificationTitle)
+                        .setContentText(notificationMessage)
+                        .setAutoCancel(true)
+                        .setSound(defaultSoundUri)
+                        .setContentIntent(pendingIntent);
+        NotificationManager notificationManager =
+                (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+
+// Since android Oreo notification channel is needed.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel(channelId,
+                    "Channel human readable title",
+                    NotificationManager.IMPORTANCE_DEFAULT);
+            notificationManager.createNotificationChannel(channel);
+        }
+        notificationManager.notify(1, notificationBuilder.build());
+    }
 
     public void checkSubscriptionForNotification(final Context context, final String deviceToken, final String senderID) {
         Log.e("Start = > ","start");
